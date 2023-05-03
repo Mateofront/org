55496f82ddf743b2ae75c7c510f052ff4e340902
Mateo <mateocorsi@gmail.com>
2023-05-03 16:34:21 -0300

    Agregue console.log

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import MiOrg from './componentes/MiOrg';
 import Equipo from './componentes/Equipo';
 import Footer from './componentes/Footer';
 
+
+console.log("Mateo")
+
 function App() {
   const [mostrarFormulario, actualizarMostrar] = useState(false)
   const [colaboradores, actualizarColaboradores] = useState([{
